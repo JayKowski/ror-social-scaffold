3d4a059effb7fe5362e561dc110a218e1fdcb7da
Elijahscriptdev <elijahobominuru@gmail.com>
2020-05-23 10:15:42 +0100

    add image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_081715) do
+ActiveRecord::Schema.define(version: 2020_05_23_082936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_081715) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
